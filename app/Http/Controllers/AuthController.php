c0b1c3015f9db16942b8f90525067649f489b3e0
RohanJdhv97 <rohan.jadhav@zysk.tech>
2024-08-09 11:02:13 +0530

    feat: Add login and register endpoints for API authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 use App\Traits\ApiResponses;
 
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login() {
-        return $this->success('Login Success');
+    public function login(LoginRequest $request) {
+        return $this->success('Login Success '. $request->email);
+    }
+
+    public function register(RegisterRequest $request){
+        return $this->success('Register Success '. $request->name);
     }
 }
