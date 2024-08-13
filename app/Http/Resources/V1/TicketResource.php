f40010e5ae2d61c2e629b85b90b95dab29dc6961
RohanJdhv97 <rohan.jadhav@zysk.tech>
2024-08-14 01:07:39 +0530

    feat: Add UserController for API/V1 namespace

--- app/Http/Resources/V1/TicketResource.php
+++ app/Http/Resources/V1/TicketResource.php
@@ -19,7 +19,7 @@ class TicketResource extends JsonResource
             'id' => $this->id,                      //Unique identifier for the resource
             'attributes' => [                       //The attributes of the resource
                 'title' => $this->title,
-                'description' => $this->description,
+                'description' => $this->when($request->routeIs('tickets.show'), $this->description),
                 'status' => $this->status,
                 'created_at' => $this->created_at,
                 'updated_at' => $this->updated_at,
